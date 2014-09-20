4b8d15f632d14986768efb61ed4159ca8d1b546c
Alexandre Isoard <alexandre.isoard@gmail.com>
2014-09-20 20:35:59 +0200

    Remove useless "let _ = ...".

--- interpreter/main.ml
+++ interpreter/main.ml
@@ -1,3 +1,3 @@
-let _ = print_endline "Grmbl !";;
-let _ = Cli.parse_arguments;;
-let _ = Parser.program Lexer.token (Lexing.from_channel stdin);;
+print_endline "Grmbl !";;
+Cli.parse_arguments;;
+Parser.program Lexer.token (Lexing.from_channel stdin);;
